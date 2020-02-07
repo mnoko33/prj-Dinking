a70ac25bc7993d41c802862f74e539afb069f42a
mnoko <mnoko92@gmail.com>
2020-02-07 14:01:18 +0900

    init signup feature

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class Signup extends Component {
     render() {
         return <div>
-            Signup
+            Signup page
         </div>
     }
 }
